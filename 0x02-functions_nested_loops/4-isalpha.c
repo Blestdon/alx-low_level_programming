f4071b795b3bcde6e54475a8716bdec995f5265d
Blestdon <blestchinonso@gmail.com>
2022-10-19 04:33:33 -0700

    Task 4. checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alpha
+ *@c: char to be checked
+ * Return: 1 if letter is lowercase, 0 if not
+ */
+int _isalpha(int c)
+{
+
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+
+		return (1);
+
+	else
+		return (0);
+
+}
