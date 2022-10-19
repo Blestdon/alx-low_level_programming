e94be16bb50302c88dbcde4a115e8e62f46fd1dc
Blestdon <blestchinonso@gmail.com>
2022-10-18 21:19:39 -0700

    Task 2. prints 10 times the alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints alphabet 10 times
+ * print_alphabet_x10 - prints the alphabet 10 times
  */
 void print_alphabet_x10(void)
 {
-	int 10;
-	char lett;
+	int q;
+	int lett;
 
-	for (ten = 0; ten <= 9; ten++)
+
+	for (q = 0; q <= 9; q++)
 	{
 		for (lett = 'a'; lett <= 'z'; lett++)
 			_putchar(lett);
-		_putchar('\n')
+		_putchar('\n');
 	}
-
 }
