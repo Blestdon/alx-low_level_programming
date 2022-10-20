5775762634a3c16905e9d9fa5395d1d27b4ad760
Blestdon <blestchinonso@gmail.com>
2022-10-19 20:34:12 -0700

    Task 100. prints the n times table, starting with 0.

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - prints the n times table, starting with 0.
+ * @n:prints the value of the time take.
+ */
+void print_times_table(int n)
+{
+	int ta, ku, asa;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (ta = 0; ta <= n; ta++)
+		{
+			_putchar('0');
+			for (ku = 1; ku <= n; ku++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				asa =  ta * ku;
+				if (asa <= 99)
+					_putchar(' ');
+				if (asa <= 9)
+					_putchar(' ');
+
+				if (asa <= 200)
+				{
+					_putchar((asa / 100) + '0');
+					_putchar(((asa / 10)) % 10 + '0');
+				}
+				else if (asa <= 99 && asa >= 10)
+				{
+					_putchar((asa / 10) + '0');
+				}
+				_putchar((asa % 10) + '0');
+			}
+			_putchar((asa % 10) + '0');
+		}
+		_putchar('\n');
+	}
+
+
+
