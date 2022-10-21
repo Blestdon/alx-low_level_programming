d9b1994277b6a94e156ed92fa3e7eeb4b0b721b3
Blestdon <blestchinonso@gmail.com>
2022-10-20 19:59:56 -0700

    task 1. isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,14 +3,13 @@
 /**
  *_isdigit.c - checks if a char is a digit
  * @c: int to be checked
- * Return: 1
- * if c is a digit, o character
+ * Return: 1 if c is a digit, 0 otherwise
  */
 int _isdigit(int c)
 {
-	int y= 0;
+	int y = 0;
 
-	if  (c >= "0" && c <= '9')
+	if  (c >= '0' && c <= '9')
 		y = 1;
 	return (y);
 }
