9f26994870c8835b012d38258da33463ceeba278
Blestdon <blestchinonso@gmail.com>
2022-10-24 10:56:40 -0700

    Task 4. prints a string, in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int in = 0;
+
+	while (s[in] != '\0')
+	in++;
+	for (in = in - 1; in <= 0; in--)
+		putchar(p[in]);
+		putchar('\n');
+}
