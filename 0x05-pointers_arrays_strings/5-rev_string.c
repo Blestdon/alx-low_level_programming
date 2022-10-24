8c91c6eaa429baea8bd22f0df1612d838d21cc80
Blestdon <blestchinonso@gmail.com>
2022-10-24 11:44:50 -0700

    Task 5. reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: input
+ * Return: Reversed sting
+ */
+void rev_string(char *s)
+{
+	char rv = s[0];
+	int a = 0;
+	int i;
+
+	while (s[a] != '\0')
+		c++;
+		for (i = 0; i < c; i++)
+		{
+			a--;
+			re = s[i];
+			s[i] = s[a];
+			s[c] = rv;
+		}
+}
