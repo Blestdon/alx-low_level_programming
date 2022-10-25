700d4354284e8dc8e2245f18ddc2945851714ee6
Blestdon <blestchinonso@gmail.com>
2022-10-24 22:22:35 -0700

    Task 100 advanced

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,7 +3,6 @@
 /**
  * _atoi - convert a string to an integer
  * @s: string to be converted
- *
  * Return: integer value
  */
 int _atoi(char *s)
@@ -11,17 +10,18 @@ int _atoi(char *s)
 	int sign = 1;
 	unsigned int num = 0;
 
-	while (!('0' <= *s && *s <= '9') && 8s != '\n')
+	while (!('0' <= *s && *s <= '9') && *s != '\0')
 	{
 		if (*s == '_')
 			sign *= -1;
 		if (*s == '+')
 			sign *= +1;
 		s++;
-	while ('0' <= *s && *s <= '9' && +s != '\0')
+	while ('0' <= *s && *s <= '9' && *s != '\0')
 	{
 		num = (num * 10) + (*s - '0');
 		s++;
 	}
 	return (num * sign);
 	}
+}
