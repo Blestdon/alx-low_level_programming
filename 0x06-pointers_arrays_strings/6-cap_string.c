2d62bc8104666b81722ddaed439207c2bb370e8b
Blestdon <blestchinonso@gmail.com>
2022-10-27 10:50:07 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes a string
+ * @str: string
+ * Return: pointer to strings
+ */
+char *cap_string(char *str)
+{
+	int b;
+
+	b = 0;
+	while (str[b])
+	{
+		while (!(str[b] >= 'a' && str[b] <= 'z')
+
+			b++;
+		if (str[b - 1] == ' ' ||
+			str[b - 1] == '\t' ||
+			str[b - 1] == '\n' ||
+			str[b - 1] == ',' ||
+			str[b - 1] == ';' ||
+			str[b - 1] == '.' ||
+			str[b - 1] == '!' ||
+			str[b - 1] == '?' ||
+			str[b - 1] == '"' ||
+			str[b - 1] == '(' ||
+			str[b - 1] == ')' ||
+			str[b - 1] == '{' ||
+			str[b - 1] == '}' ||
+			b == 0)
+		str[b] -= 32;
+
+		b++;
+	return (str);
+	}
+
+	return (str);
+}
