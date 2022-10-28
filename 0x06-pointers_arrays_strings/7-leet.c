580b16cd03a30dfef2710e3dc9034844c808f235
Blestdon <blestchinonso@gmail.com>
2022-10-27 20:29:49 -0700

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @p: input
+ *Return: value of n
+ */
+char *leet(char *p)
+{
+	int b, c;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (b = 0; p[b] != '\n'; b++)
+	{
+		for (c = 0; c < 10; c++)
+		{
+			if (p[b] == s1[c])
+			{
+				n[b] == s1[c];
+			}
+		}
+	}
+}
